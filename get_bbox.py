63a25950a25e94890a80aee335ad5bccbbc76560
ranxiao <ben.xiao@me.com>
2019-10-27 12:09:09 +1100

    remove __main__

--- get_bbox.py
+++ get_bbox.py
@@ -1,8 +1,6 @@
 from collections import deque
 from typing import *
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from bbox import BoundingBox
 
 Point = Tuple[int, int]
@@ -76,27 +74,3 @@ def bboxes(square: Square, dimension: Dimension, threshold=200):
             if p in candidates:
                 candidates.remove(p)
     return lst
-
-
-def bbox_filter_size(bs: List[BoundingBox], minimum_size=10):
-    return [b for b in bs if b.area() > minimum_size]
-
-
-
-if __name__ == '__main__':
-    # print(bfs(square, (5,5), (0, 2)))
-    img = cv2.imread("gray.jpg")
-    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # print(gray_img.flatten().min(), gray_img.flatten().max())
-    # print(np.unique(gray_img.flatten()))
-    lst = gray_img.tolist()
-    bs = bboxes(gray_img, gray_img.shape)
-    bs = bbox_filter_size(bs, minimum_size=20)
-    bs = [b.scale(0.2, 0.1, gray_img.shape) for b in bs]
-    for b in bs:
-        cv2.rectangle(img, *(b.to_opencv_format()), (255, 0, 0), 1)
-
-
-
-    plt.imshow(img)
-    plt.show()
