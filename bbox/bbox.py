23787fe01be9f6fe431b3ba458e3681f5fa6fa4a
ranxiao <ben.xiao@me.com>
2019-10-30 21:58:01 +1100

    use dimension instead of x y in BoundingBox class

--- bbox/bbox.py
+++ bbox/bbox.py
@@ -1,79 +1,82 @@
 from typing import *
 
-X, Y = 0, 1
+D0, D1 = 0, 1
 Point = Tuple[int, int]
+
+
+"""
+Point d0: Dimension 0, d1: Dimension 1
+
+"""
 
 
 class BoundingBox:
     def __init__(self, top_left: Point, bottom_right: Point):
-        self.top_left = top_left
-        self.bottom_right = bottom_right
-        if self.bottom_right[X] < self.top_left[X] and self.bottom_right[Y] < self.top_left[Y]:
-            raise ValueError("invalid boundbox")
+        self._top_left = top_left
+        self._bottom_right = bottom_right
+        # if not self.validate():
+        #     print(self._top_left)
+        #     print(self._bottom_right)
+        #     raise ValueError("invalid boundbox")
+
+    def validate(self):
+        return self._bottom_right[D0] >= self._top_left[D0] and self._bottom_right[D1] >= self._top_left[D1]
+
+    def __str__(self):
+        return f"BoundingBox({self._top_left}, {self._bottom_right})"
+
+    __repr__ = __str__
 
     def to_opencv_format(self):
-        return self.top_left, self.bottom_right
+        return self.get_top_left(), self.get_bottom_right()
 
     def get_top_left(self):
-        return self.top_left
+        return self._top_left
 
     def get_bottom_right(self):
-        return self.bottom_right
+        return self._bottom_right
 
     def get_bottom_left(self):
-        return self.top_left[X], self.bottom_right[Y]
+        return self.get_bottom_right()[D0], self.get_top_left()[D1]
 
     def get_top_right(self):
-        return self.bottom_right[X], self.top_left[Y]
+        return self._bottom_right[D0], self._top_left[D1]
 
     def width(self):
-        return self.bottom_right[X] - self.top_left[X]
+        return self._bottom_right[D0] - self._top_left[D0]
 
     def height(self):
-        return self.bottom_right[Y] - self.top_left[Y]
+        return self._bottom_right[D1] - self._top_left[D1]
 
     def shape(self):
-        return self.width(), self.height()
+        return self.height(), self.width()
 
     def area(self):
         return self.width() * self.height()
 
     def center(self):
-        x0, y0 = self.get_top_left()
-        return x0 + self.width() // 2,  y0 + self.height() // 2
+        d0, d1 = self.get_top_left()
+        return d0 + self.height() // 2,  d1 + self.width() // 2
 
     def scale(self, w_factor, h_factor, context):
-        context_width, context_height = context
+        context_height, context_width = context
         width = self.width()
         height = self.height()
         top_left = self.get_top_left()
         bottom_right = self.get_bottom_right()
-        top_left_x = max(top_left[X] - width * w_factor, 0)
-        top_left_y = max(top_left[Y] - height * h_factor, 0)
-        bottom_right_x = min(bottom_right[X] + width * w_factor, context_width)
-        bottom_right_y = min(bottom_right[Y] + height * h_factor, context_height)
-        return BoundingBox((int(top_left_x), int(top_left_y)),
-                           (int(bottom_right_x), int(bottom_right_y)))
+        top_left_d0 = max(top_left[D0] - height * h_factor, 0)
+        top_left_d1 = max(top_left[D1] - width * w_factor, 0)
+        bottom_right_d0 = min(bottom_right[D0] + height * h_factor, context_width)
+        bottom_right_d1 = min(bottom_right[D1] + width * w_factor, context_height)
+        return BoundingBox((int(top_left_d0), int(top_left_d1)),
+                           (int(bottom_right_d0), int(bottom_right_d1)))
 
-    def merge(self, other):
-        top_left0 = self.get_top_left()
-        top_left1 = other.get_top_left()
-        bottom_right0 = self.get_bottom_right()
-        bottom_right1 = other.get_bottom_right()
-        new_top_left = (
-            min(top_left0[X], top_left1[X]),
-            min(top_left0[Y], top_left1[Y])
-        )
-        new_bottom_right = (
-            max(bottom_right0[X], bottom_right1[X]),
-            max(bottom_right0[Y], bottom_right1[Y])
-        )
-        return BoundingBox(new_top_left, new_bottom_right)
-
+    # maybe there are bugs
     def overlap(self, other):
+        # how to test this
         a, b = self, other
         # a always stays on the left
-        if a.top_left[X] > b.top_left[Y]:
+        if a.get_top_left()[D1] > b.get_top_left()[D1]:
             a, b = b, a
         x0, y0 = a.get_bottom_right()
         x1, y1 = b.get_top_left()
@@ -85,5 +88,7 @@ class BoundingBox:
 
 
 if __name__ == '__main__':
-    pass
+    a = BoundingBox((0, 0), (400, 400))
+    b = BoundingBox((400,400), (401,401))
+    print(a.overlap(b))
 
